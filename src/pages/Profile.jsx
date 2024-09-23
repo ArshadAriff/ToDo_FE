661a4cb0f30181895aaffb397b0a73c585c6afd2
ArshadAriff <113685884+ArshadAriff@users.noreply.github.com>
2024-09-23 18:29:18 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,10 @@ import Loader from "../components/Loader";
 const Profile = () => {
   const { isAuthenticated, loading, user } = useContext(Context);
 //  if (!isAuthenticated) return <Navigate to={"/login"} />;
+    if (!user) {
+    return <div>No user data available.</div>; // Handle case where user data is empty
+  }
+
 
   return loading ? (
     <Loader />
