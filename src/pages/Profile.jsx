43e20b97f9afb93b83c0692a5de3f7084b88cc26
ArshadAriff <113685884+ArshadAriff@users.noreply.github.com>
2024-09-24 11:54:18 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { Context } from "../main";
 import Loader from "../components/Loader";
+import { Navigate   } from "react-router-dom";
 
 const Profile = () => {
   const { isAuthenticated, loading, user } = useContext(Context);
+  if (!isAuthenticated) return <Navigate to={"/login"} />;
 
 
   return loading ? (
